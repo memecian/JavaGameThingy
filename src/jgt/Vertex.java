b0a962850bcd046bc347b034cffca8b7a9d97b2e
AlexanderKomyakov <AlexanderKomyakov@0-208-09>
2020-12-04 10:42:06 +0100

    04122020 - Commit I

--- src/jgt/Vertex.java
+++ src/jgt/Vertex.java
@@ -5,20 +5,53 @@ public class Vertex {
 	public double x;
 	public double y;
 
+	// Constructor
 	public Vertex(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public double length() {
-		return Math.sqrt(x*x+y*y);
+		return Math.sqrt(x * x + y * y);
 	}
-	
-	public Vertex scalarMult(double s) {
-		return new Vertex(x*s, y*s);
+// modifiers --------------------------------
+	public void scalarMultMod(double s) {
+		x = x * s;
+		y = y * s;
 	}
+
+	public void addMod(Vertex v) {
+		x=x+v.x;
+		y=y+v.y;
+	}
+// ------------------------------------------
 	
 	public String toString() {
-		return "("+x+","+y+")";
+		return "(" + x + "," + y + ")";
+	}
+
+// getters/setters --------------------------
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	
+	public void setX(double val) {
+		x = val;
+	}
+	public void setY(double val) {
+		y = val;
+	}
+// ------------------------------------------
+	
+// ------------------------------------------	
+	public boolean equals(Object thatObject) {
+		if (thatObject instanceof Vertex) {
+			Vertex that = (Vertex)thatObject;
+			return this.x == that.x && this.y == that.y;
+		}
+		return false;
 	}
 }
